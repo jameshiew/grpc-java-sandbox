467bb165120a19ebf2804d0d7ddf272cd890dd03
James Hiew <james@hiew.net>
2019-05-18 12:34:47 +0100

    Close call cleanly and then return a noop listener

--- src/main/java/sandbox/server/interceptors/AuthenticationInterceptor.java
+++ src/main/java/sandbox/server/interceptors/AuthenticationInterceptor.java
@@ -1,11 +1,11 @@
 package sandbox.server.interceptors;
 
+import io.grpc.ForwardingServerCallListener;
 import io.grpc.Metadata;
 import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
 import io.grpc.ServerInterceptor;
 import io.grpc.Status;
-import io.grpc.StatusRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,8 +20,24 @@ public final class AuthenticationInterceptor implements ServerInterceptor {
   public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
       ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
     if (!EXPECTED_AUTH_TOKEN.equals(headers.get(AUTH_TOKEN_HEADER))) {
-      throw new StatusRuntimeException(Status.UNAUTHENTICATED);
+      return close(call, Status.UNAUTHENTICATED);
     }
     return next.startCall(call, headers);
+  }
+
+  private static <ReqT, RespT> ServerCall.Listener<ReqT> close(
+      ServerCall<ReqT, RespT> call, Status status) {
+    call.close(status, new Metadata());
+    return new ForwardingServerCallListener<ReqT>() {
+      @Override
+      protected ServerCall.Listener<ReqT> delegate() {
+        return new ServerCall.Listener<ReqT>() {
+          @Override
+          public void onMessage(ReqT message) {
+            return;
+          }
+        };
+      }
+    };
   }
 }
