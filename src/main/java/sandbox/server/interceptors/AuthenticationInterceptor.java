ab7044a41195538f65de26468830b06a81fcbe39
James Hiew <james@hiew.net>
2019-05-18 12:21:38 +0100

    Server authentication interceptor expects 1234 in call header

--- src/main/java/sandbox/server/interceptors/AuthenticationInterceptor.java
+++ src/main/java/sandbox/server/interceptors/AuthenticationInterceptor.java
@@ -10,11 +10,15 @@ import org.slf4j.LoggerFactory;
 public final class AuthenticationInterceptor implements ServerInterceptor {
   private static final Logger logger = LoggerFactory.getLogger(AuthenticationInterceptor.class);
 
+  private static final String EXPECTED_AUTH_TOKEN = "1234";
+  private static final Metadata.Key<String> AUTH_TOKEN_HEADER =
+      Metadata.Key.of("AUTH_TOKEN", Metadata.ASCII_STRING_MARSHALLER);
+
   @Override
   public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
       ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
-    if (!headers.keys().contains("AUTH_TOKEN")) {
-      throw new RuntimeException("No auth token supplied!");
+    if (!EXPECTED_AUTH_TOKEN.equals(headers.get(AUTH_TOKEN_HEADER))) {
+      throw new RuntimeException("Unauthenticated!");
     }
     return next.startCall(call, headers);
   }
