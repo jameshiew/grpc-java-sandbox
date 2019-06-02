7eba9a3575442950fa4cf26bdf3277d8c1c7018f
James Hiew <james@hiew.net>
2019-06-02 15:30:39 +0100

    Enable JUnit 4

--- src/test/java/sandbox/colors/ServiceTest.java
+++ src/test/java/sandbox/colors/ServiceTest.java
@@ -1,19 +1,36 @@
 package sandbox.colors;
 
+import io.grpc.BindableService;
+import io.grpc.StatusRuntimeException;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
 import io.grpc.testing.GrpcCleanupRule;
+import net.hiew.sandbox.colors.ColorsGrpc;
+import net.hiew.sandbox.colors.ColorsOuterClass;
 import org.junit.Rule;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
 
-@EnableRuleMigrationSupport
-class ServiceTest {
-  @Rule
-  public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+public class ServiceTest {
+  @Rule public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+
+  private final String serverName = InProcessServerBuilder.generateName();
+  private final InProcessServerBuilder serverBuilder =
+      InProcessServerBuilder.forName(serverName).directExecutor();
+  private final InProcessChannelBuilder channelBuilder =
+      InProcessChannelBuilder.forName(serverName).directExecutor();
 
   @Test
-  void testOnePlusOne() {
-    assertEquals(2, 1 + 1, "test");
+  public void testGetColor() throws IOException {
+    final BindableService service = new Service();
+    grpcCleanup.register(serverBuilder.addService(service).build().start());
+    final var channel = grpcCleanup.register(channelBuilder.maxInboundMessageSize(1024).build());
+    ColorsGrpc.ColorsBlockingStub stub = ColorsGrpc.newBlockingStub(channel);
+    try {
+      stub.getColor(ColorsOuterClass.GetColorRequest.newBuilder().build());
+    } catch (StatusRuntimeException e) {
+
+    }
   }
 }
