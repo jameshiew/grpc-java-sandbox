e5b6c4b7281d461fa37a471f68e7d74a26e10e1c
James Hiew <james@hiew.net>
2019-06-02 15:38:11 +0100

    Some tests

--- src/test/java/sandbox/colors/ServiceTest.java
+++ src/test/java/sandbox/colors/ServiceTest.java
@@ -1,6 +1,6 @@
 package sandbox.colors;
 
-import io.grpc.BindableService;
+import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
@@ -8,11 +8,16 @@ import io.grpc.testing.GrpcCleanupRule;
 import net.hiew.sandbox.colors.ColorsGrpc;
 import net.hiew.sandbox.colors.ColorsOuterClass;
 import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport;
 
 import java.io.IOException;
 
-public class ServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@EnableRuleMigrationSupport
+class ServiceTest {
   @Rule public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
 
   private final String serverName = InProcessServerBuilder.generateName();
@@ -22,15 +27,41 @@ public class ServiceTest {
       InProcessChannelBuilder.forName(serverName).directExecutor();
 
   @Test
-  public void testGetColor() throws IOException {
-    final BindableService service = new Service();
-    grpcCleanup.register(serverBuilder.addService(service).build().start());
-    final var channel = grpcCleanup.register(channelBuilder.maxInboundMessageSize(1024).build());
-    ColorsGrpc.ColorsBlockingStub stub = ColorsGrpc.newBlockingStub(channel);
-    try {
-      stub.getColor(ColorsOuterClass.GetColorRequest.newBuilder().build());
-    } catch (StatusRuntimeException e) {
+  void testGetColorInitiallyUnavailable() throws IOException {
+    grpcCleanup.register(serverBuilder.addService(new Service()).build().start());
+    ColorsGrpc.ColorsBlockingStub stub =
+        ColorsGrpc.newBlockingStub(
+            grpcCleanup.register(channelBuilder.maxInboundMessageSize(1024).build()));
 
+    for (var i = 0; i < 2; i++) {
+      final var status =
+          assertThrows(
+                  StatusRuntimeException.class,
+                  () -> stub.getColor(ColorsOuterClass.GetColorRequest.newBuilder().build()))
+              .getStatus();
+      assertEquals(Status.UNAVAILABLE, status, "initially unavailable");
     }
+  }
+
+  @Test
+  void testGetColorFinallySucceeds() throws IOException {
+    grpcCleanup.register(serverBuilder.addService(new Service()).build().start());
+    ColorsGrpc.ColorsBlockingStub stub =
+        ColorsGrpc.newBlockingStub(
+            grpcCleanup.register(channelBuilder.maxInboundMessageSize(1024).build()));
+    for (var i = 0; i < 2; i++) {
+      try {
+        stub.getColor(ColorsOuterClass.GetColorRequest.newBuilder().build());
+      } catch (StatusRuntimeException e) {
+
+      }
+    }
+
+    final var response = stub.getColor(ColorsOuterClass.GetColorRequest.newBuilder().build());
+
+    assertEquals(
+        ColorsOuterClass.Color.newBuilder().setRed(100).setBlue(100).setGreen(100).build(),
+        response,
+        "finally succeeds");
   }
 }
